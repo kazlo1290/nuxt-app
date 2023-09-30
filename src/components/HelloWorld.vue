f125de11f41d96e35b75c42c4e1e40aafa748064
kazlo1290 <69760790+kazlo1290@users.noreply.github.com>
2023-09-30 17:36:47 +0800

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
+<template>
+    <div class="hello">
+        Hello Nuxt {{ version }}!
+    </div>
+</template>
+
+<style scoped>
+.hello {
+    font-size: 3rem;
+    padding: 2rem;
+    text-align: center;
+    font-weight: 600;
+}
+</style>
